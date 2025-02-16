374fb4291cad2f042883275a433ac1943a7aae11
Shilpa Merlin joy <shilpamjoy@gmail.com>
2025-02-16 11:17:44 -0500

    Done with the integration of RevenueCat

--- RevenueCatDemo/AppDelegate.swift
+++ RevenueCatDemo/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RevenueCat
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        Purchases.logLevel = .debug
+        Purchases.configure(withAPIKey: "appl_yMzClbkcVpbVyQQMKVLoCQQDnhe")
         return true
     }
 
--- RevenueCatDemo/RecipeViewController.swift
+++ RevenueCatDemo/RecipeViewController.swift
@@ -11,11 +11,30 @@ import RevenueCatUI
 
 class RecipeViewController: UIViewController {
 
+    @IBOutlet weak var recipeImageView: UIImageView!
+    @IBOutlet weak var saveRecipeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        recipeImageView.layer.cornerRadius = 5
+        saveRecipeButton.layer.cornerRadius = 10
+        navigationItem.title = "Recipes"
+       
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
+    
+   
+    @IBAction func saveRecipe(_ sender: Any) {
+        let controller = PaywallViewController()
+            controller.delegate = self
+            present(controller, animated: true, completion: nil)
+    }
+}
 
+extension RecipeViewController: PaywallViewControllerDelegate {
 
+    func paywallViewController(_ controller: PaywallViewController,
+                               didFinishPurchasingWith customerInfo: CustomerInfo) {
+    }
 }
 
